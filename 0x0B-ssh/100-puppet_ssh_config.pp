4f147fcab87164c72a082ffbc2a003e7c4fe5bbc
deking254 <dekingsky@gmail.com>
2023-08-27 10:38:29 +0000

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,5 @@
-file {'/etc/ssh/ssh_config.d/r':
+#makes sure that the config file is present
+file {'/etc/ssh/ssh_config.d/r.conf':
 ensure  => present,
 mode    => '0600',
 content => 'PasswordAuthentication no

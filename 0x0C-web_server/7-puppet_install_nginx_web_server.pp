71300536f45d27a6fd70d66e9f0d37788718354c
deking254 <dekingsky@gmail.com>
2023-09-04 11:58:18 +0000

    7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,11 +4,13 @@ ensure => installed
 }
 file {'/var/www/html/404.html':
 ensure  => present,
-content => "Ceci n'est pas une page",
+content => "Ceci n'est pas une page
+",
 }
 file {'/var/www/html/index.html':
 ensure  => present,
-content => "Hello World!",
+content => "Hello World!
+",
 }
 file {'/etc/nginx/sites-enabled/default':
 content => "server {
@@ -16,7 +18,7 @@ content => "server {
 	root /var/www/html;
 	server_name _;
 	error_page 404 /404.html;
-	index index.html}
+	index index.html;
 	location /redirect_me {
 		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
 }

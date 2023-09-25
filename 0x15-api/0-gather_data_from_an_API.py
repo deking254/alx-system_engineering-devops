a2aa930a055b90ffdd279562cbfba56575b13f9a
deking254 <dekingsky@gmail.com>
2023-09-25 07:17:46 +0000

    cs

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import requests
+import sys
+"""this is an api call program"""
+if __name__ == "__main__":
+    i = sys.argv[1]
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(i)
+    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(i)
+    c = 0
+    tasks = requests.get(url).json()
+    user = requests.get(user_url).json().get("name")
+    t = len(tasks)
+    for task in tasks:
+        if task.get("completed"):
+            c = c + 1
+    print("Employee {} is done with tasks({}/{}):".format(user, c, t))
+    for task in tasks:
+        if task.get("completed"):
+            print("\t " + task.get("title"))

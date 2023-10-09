823fa12d13635feab072dd10f173d75e5afb0447
deking254 <dekingsky@gmail.com>
2023-10-09 16:33:59 +0000

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""queries the Reddit API and prints the titles of the first 10 hot posts"""
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titiles of the first 10 posts"""
+    data = requests.get("https://www.reddit.com/r/"
+                        + subreddit + "/hot.json?limit=10").json()
+    try:
+        for title in data["children"]:
+            print(title["data"]["title"])
+    except Exception as e:
+        return None

493bb1f03ed08d2a8b7b48ab55120815d0c2d1f5
deking254 <dekingsky@gmail.com>
2023-10-09 15:37:43 +0000

    all

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,6 +3,7 @@
 import urllib.request as urllib
 import json
 
+
 def number_of_subscribers(subreddit):
     """returns the no of subscribers to subreddit and 0 if nothing"""
     CLIENT_ID = "u13OXnhDSKr0SR42N1-L5WqVKJ3KuQ"
@@ -11,10 +12,13 @@ def number_of_subscribers(subreddit):
     SCOPE_STRING = "[wikiread]"
     URI = "http://127.0.0.1:65010/authorize_callback"
     DURATION = "permanent"
-    urllib.urlopen("https://www.reddit.com/api/v1/authorize?client_id=" + CLIENT_ID + "&response_type=" + TYPE + "&state=" + 
-            RANDOM_STRING + "&redirect_uri=" + URI + "&duration=" 
-            + DURATION + "&scope=" + SCOPE_STRING)
+    urllib.urlopen("https://www.reddit.com/api/v1/authorize?client_id="
+                   + CLIENT_ID
+                   + "&response_type=" + TYPE + "&state=" +
+                   RANDOM_STRING + "&redirect_uri=" + URI + "&duration="
+                   + DURATION + "&scope=" + SCOPE_STRING)
     if subreddit:
-        data = urllib.urlopen("https://api.reddit.com/r/" + subreddit + "/about")
+        data = urllib.urlopen("https://api.reddit.com/r/"
+                              + subreddit + "/about")
         tr = json.loads(data.read())
         return tr["data"]["subscribers"]
